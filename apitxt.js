f95f9563d0025b8cd645bb11826327947c3c72ac
Nikhil-75 <nikhilpandey4591@gmail.com>
2023-02-07 13:33:48 +0530

    update

--- apitxt.js
+++ apitxt.js
@@ -41,7 +41,7 @@ const server = http.createServer((req, res) => {
 
   if (req.method === "GET") {
     res.end("get method");
-    req.on("data", gatdata);
+    req.on("data", getdata);
     return;
 
   }
@@ -49,7 +49,7 @@ const server = http.createServer((req, res) => {
     res.end("post method ");
     req.on("data",postdata)
     return;
-  }
+  } 
   if (req.method === "PUT") {
     res.end("put method ");
     req.on("data", pushdata);
